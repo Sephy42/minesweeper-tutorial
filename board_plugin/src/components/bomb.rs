94a5c263a8ea44fbc667b57dd63434cf21f0a906
margot <margot.cantaloube@gmail.com>
2022-06-03 10:31:14 +0200

    part 4 OK

---
+++ board_plugin/src/components/bomb.rs
@@ -0,0 +1,7 @@
+// bomb.rs
+use bevy::prelude::Component;
+
+/// Bomb component
+#[cfg_attr(feature = "debug", derive(bevy_inspector_egui::Inspectable))]
+#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Component)]
+pub struct Bomb;
--- board_plugin/src/components/mod.rs
+++ board_plugin/src/components/mod.rs
@@ -1,4 +1,10 @@
 // board_plugin/components/mod.rs
+pub use bomb::Bomb;
+pub use bomb_neighbor::BombNeighbor;
 pub use coordinates::*;
+pub use uncover::Uncover;
 
+mod bomb;
+mod bomb_neighbor;
 mod coordinates;
+mod uncover;
