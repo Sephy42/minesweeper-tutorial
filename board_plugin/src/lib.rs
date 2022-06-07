96998d3902d1675702f77942504724aad913d194
margot <margot.cantaloube@gmail.com>
2022-06-07 11:36:15 +0200

    part 9 OK

--- board_plugin/src/lib.rs
+++ board_plugin/src/lib.rs
@@ -10,7 +10,7 @@ use crate::components::*;
 use crate::events::*;
 use crate::resources::tile::Tile;
 use crate::resources::tile_map::TileMap;
-use crate::resources::{Board, BoardOptions, BoardPosition, TileSize};
+use crate::resources::{Board, BoardAssets, BoardOptions, BoardPosition, TileSize};
 use bevy::ecs::schedule::StateData;
 use bevy::log;
 use bevy::math::Vec3Swizzles;
@@ -61,14 +61,12 @@ impl<T> BoardPlugin<T> {
         mut commands: Commands,
         board_options: Option<Res<BoardOptions>>,
         window: Res<WindowDescriptor>,
-        asset_server: Res<AssetServer>, // The AssetServer resource
+        board_assets: Res<BoardAssets>,
     ) {
         let options = match board_options {
             None => BoardOptions::default(), // If no options is set we use the default one
             Some(o) => o.clone(),
         };
-        let font = asset_server.load("fonts/pixeled.ttf");
-        let bomb_image = asset_server.load("sprites/bomb.png");
 
         // Tilemap generation
         let mut tile_map = TileMap::empty(options.map_size.0, options.map_size.1);
@@ -117,10 +115,11 @@ impl<T> BoardPlugin<T> {
                 parent
                     .spawn_bundle(SpriteBundle {
                         sprite: Sprite {
-                            color: Color::WHITE,
+                            color: board_assets.board_material.color,
                             custom_size: Some(board_size),
                             ..Default::default()
                         },
+                        texture: board_assets.board_material.texture.clone(),
                         transform: Transform::from_xyz(board_size.x / 2., board_size.y / 2., 0.),
                         ..Default::default()
                     })
@@ -130,10 +129,7 @@ impl<T> BoardPlugin<T> {
                     &tile_map,
                     tile_size,
                     options.tile_padding,
-                    Color::GRAY,
-                    bomb_image,
-                    font,
-                    Color::DARK_GRAY,
+                    &board_assets,
                     &mut covered_tiles,
                     &mut safe_start,
                 );
@@ -170,26 +166,17 @@ impl<T> BoardPlugin<T> {
     }
 
     /// Generates the bomb counter text 2D Bundle for a given value
-    fn bomb_count_text_bundle(count: u8, font: Handle<Font>, size: f32) -> Text2dBundle {
+    fn bomb_count_text_bundle(count: u8, board_assets: &BoardAssets, size: f32) -> Text2dBundle {
         // We retrieve the text and the correct color
-        let (text, color) = (
-            count.to_string(),
-            match count {
-                1 => Color::WHITE,
-                2 => Color::GREEN,
-                3 => Color::YELLOW,
-                4 => Color::ORANGE,
-                _ => Color::PURPLE,
-            },
-        );
+        let color = board_assets.bomb_counter_color(count);
         // We generate a text bundle
         Text2dBundle {
             text: Text {
                 sections: vec![TextSection {
-                    value: text,
+                    value: count.to_string(),
                     style: TextStyle {
                         color,
-                        font,
+                        font: board_assets.bomb_counter_font.clone(),
                         font_size: size,
                     },
                 }],
@@ -208,10 +195,7 @@ impl<T> BoardPlugin<T> {
         tile_map: &TileMap,
         size: f32,
         padding: f32,
-        color: Color,
-        bomb_image: Handle<Image>,
-        font: Handle<Font>,
-        covered_tile_color: Color,
+        board_assets: &BoardAssets,
         covered_tiles: &mut HashMap<Coordinates, Entity>,
         safe_start_entity: &mut Option<Entity>,
     ) {
@@ -225,10 +209,11 @@ impl<T> BoardPlugin<T> {
                 let mut cmd = parent.spawn();
                 cmd.insert_bundle(SpriteBundle {
                     sprite: Sprite {
-                        color,
+                        color: board_assets.tile_material.color,
                         custom_size: Some(Vec2::splat(size - padding)),
                         ..Default::default()
                     },
+                    texture: board_assets.tile_material.texture.clone(),
                     transform: Transform::from_xyz(
                         (x as f32 * size) + (size / 2.),
                         (y as f32 * size) + (size / 2.),
@@ -244,9 +229,10 @@ impl<T> BoardPlugin<T> {
                         .spawn_bundle(SpriteBundle {
                             sprite: Sprite {
                                 custom_size: Some(Vec2::splat(size - padding)),
-                                color: covered_tile_color,
+                                color: board_assets.covered_tile_material.color,
                                 ..Default::default()
                             },
+                            texture: board_assets.covered_tile_material.texture.clone(),
                             transform: Transform::from_xyz(0., 0., 2.),
                             ..Default::default()
                         })
@@ -268,7 +254,7 @@ impl<T> BoardPlugin<T> {
                                     ..Default::default()
                                 },
                                 transform: Transform::from_xyz(0., 0., 1.),
-                                texture: bomb_image.clone(),
+                                texture: board_assets.bomb_material.texture.clone(),
                                 ..Default::default()
                             });
                         });
@@ -279,7 +265,7 @@ impl<T> BoardPlugin<T> {
                         cmd.with_children(|parent| {
                             parent.spawn_bundle(Self::bomb_count_text_bundle(
                                 *v,
-                                font.clone(),
+                                board_assets,
                                 size - padding,
                             ));
                         });
--- board_plugin/src/resources/mod.rs
+++ board_plugin/src/resources/mod.rs
@@ -2,6 +2,8 @@ mod board;
 mod board_options;
 pub mod tile;
 pub mod tile_map;
+mod board_assets;
 
 pub use board::*;
 pub use board_options::*;
+pub use board_assets::*;
